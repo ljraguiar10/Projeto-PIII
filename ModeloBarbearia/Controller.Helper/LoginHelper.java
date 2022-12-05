07b0d6a36f6f281ec4874f8a9fc0ecf9f7ed5180
luizcbrito <101910843+luizcbrito@users.noreply.github.com>
2022-12-04 21:32:36 -0300

    Adição das classes no Controller.Helper

--- ModeloBarbearia/Controller.Helper/LoginHelper.java
+++ ModeloBarbearia/Controller.Helper/LoginHelper.java
@@ -1,41 +1,44 @@
-package Controller.Helper;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.helper;
 
-import Model.Usuário;
+import Model.Usuario;
 import View.Login;
 
-public class LoginHelper {
 
-	private final Login view;
 
-	public LoginHelper(Login view) {
-		this.view = view;
-	}
+/**
+ *
+ * @author Amanda
+ */
+public class LoginHelper implements IHelper{
+    private final Login view;
 
-	public Usuário obterModelo() {
-
-		String nome = view.getTextUsuario().getText();
-		String senha = view.getTextSenha().getText();
-
-		Usuário modelo = new Usuário(0, nome, senha);
-
-		return modelo;
-
-	}
-
-	public void setarModelo(Usuário modelo) {
-
-		String nome = modelo.getNome();
-		String senha = modelo.getSenha();
-		
-		view.getTextUsuario().setText(nome);
-		view.getTextSenha().setText(senha);
-	}
-	
-	public void limparTela() {
-		
-		view.getTextUsuario().setText("");
-		view.getTextSenha().setText("");
-		
-	}
-
+    public LoginHelper(Login view) {
+        this.view = view;
+    }
+    
+    public Usuario obterModelo(){
+     
+        String nome = view.getTextUsuario().getText();
+        String senha = view.getTextSenha().getText();
+        Usuario modelo = new Usuario(senha, 0, nome);
+        return modelo;
+    }
+    
+    public void setarModelo(Usuario modelo){
+        String nome = modelo.getNome();
+        String senha = modelo.getSenha();
+        
+        view.getTextUsuario().setText(nome);
+        view.getTextSenha().setText(senha);
+    }
+    
+    public void limparTela(){
+        view.getTextUsuario().setText("");
+        view.getTextSenha().setText("");
+    }
 }
